d2e8d52c08b1c0050eca1bba5cb527dcc7744b79
Snanttu <santeri.orava@hotmail.com>
2018-11-16 14:01:38 +0200

    Initial commit

---
+++ Unity Tutorials/Spees/Assets/_Scripts/Out_of_bounds.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Out_of_bounds : MonoBehaviour {
+
+	void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
---
+++ Unity Tutorials/Spees/Assets/_Scripts/Random_rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random_rotator : MonoBehaviour {
+
+    public float tumble;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
